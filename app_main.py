0b31969c8fcc254836980219443d54f68827d18e
NishadMoodley786 <ST10410007@vcconnect.edu.za>
2025-02-04 12:41:16 +0200

    styling

--- app_main.py
+++ app_main.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pathlib
+
+#load css file
+def load_css(file_path):
+    with open(file_path) as f:
+        st.html(f"<style>{f.read()}</style>")
+
+#load the external css
+css_path = pathlib.Path("style.css")
+load_css(css_path)
 
 st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 st.title("AI stat maker:")
@@ -7,7 +17,7 @@ col1, col2  = st.columns(2)
 with col1:
     pass
 with col2 :
-    center_button = st.button('CLICK ME')
+    center_button = st.button('CLICK ME', key="red")
 
 if center_button:
-    st.audio("songs/SEE YOU AGAIN featuring Kali Uchis [TGgcC5xg9YI].mp3",start_time=27, autoplay=True)
+    st.audio("song   3s/SEE YOU AGAIN featuring Kali Uchis [TGgcC5xg9YI].mp3",start_time=27, autoplay=True)
