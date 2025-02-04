70af44facc95710e038fcfa0f9adfe0780d55a6e
NishadMoodley786 <ST10410007@vcconnect.edu.za>
2025-02-04 18:24:16 +0200

    Update Statistics.py

--- pages/Statistics.py
+++ pages/Statistics.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import plotly as px
+import pandas as pd
+
 
 st.title("Statistics:")
+
+df= pd.DataFrame(dict(
+    x = [1, 2, 3, 4],
+    y = [1, 2, 3, 4]
+))
+
+fig = px.line(df, x="x", y="y", title="How much I love you <3")
+
+fig.show()
 
 
