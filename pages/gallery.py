95e7f6b2d00cf5f01d82758f84ae333a692103ec
NishadMoodley786 <ST10410007@vcconnect.edu.za>
2025-02-04 19:02:03 +0200

    gALLERY CODE

---
+++ pages/gallery.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+
+st.set_page_config(page_title="Gallery", page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
+st.title("Gallery:")
+
+
+
+
