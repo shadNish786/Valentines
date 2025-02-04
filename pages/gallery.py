bdf3eb2c945bc3864b3678b304b084dad64ffb6c
NishadMoodley786 <ST10410007@vcconnect.edu.za>
2025-02-04 19:05:13 +0200

    Update gallery.py

--- pages/gallery.py
+++ pages/gallery.py
@@ -3,7 +3,13 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Gallery", page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
-st.title("Gallery:")
+
+
+
+col1,col2,col3,col4,col5 = st.columns(5)
+
+with col3:
+    st.title("Gallery:")
 
 
 
